c304902d6ca9799fdf499c5f9a21b7dd0da722b5
diegogila <dgg26496@hotmail.com>
2025-03-23 16:39:18 -0600

    Finished the import of web interface

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import json
 from flask_cors import CORS
 from chatbot import to_talk
 
 app = Flask(__name__)
 CORS(app)
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
 
 @app.route('/chatbot', methods=['POST'])
 def handle_prompt():
